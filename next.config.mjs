851f2023b805eb1fc5c010ecd6a17550e2bd6593
Rajan Thakkar <thakkarrajan10@gmail.com>
2025-07-21 09:40:47 -0400

    Implement redirects in next.config.mjs and remove legacy vercel.json configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
-// next.config.mjs
 const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.iloveresumes.ca" }],
+        destination: "https://iloveresumes.ca/:path*",
+        permanent: true,
+      },
+    ];
   },
 };
 
