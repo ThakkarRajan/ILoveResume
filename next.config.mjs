a19ad5137c3f13c4954417b4393be78e0d0696b0
Rajan Thakkar <thakkarrajan10@gmail.com>
2025-07-23 11:41:06 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com; style-src 'self' 'unsafe-inline'; frame-src 'self' https://*.firebaseapp.com https://*.firebaseio.com https://accounts.google.com https://apis.google.com; connect-src 'self' https://identitytoolkit.googleapis.com https://*.firebaseio.com https://firestore.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://firebasestorage.googleapis.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com; style-src 'self' 'unsafe-inline'; frame-src 'self' https://*.firebaseapp.com https://*.firebaseio.com https://accounts.google.com https://apis.google.com; connect-src 'self' https://identitytoolkit.googleapis.com https://*.firebaseio.com https://firestore.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://firebasestorage.googleapis.com https://jobdraftai-backend-production.up.railway.app;",
           },
           {
             key: 'X-Frame-Options',
